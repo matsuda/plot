7d4784e16ca67c9ecfffb3f33c7cc24aabb2a239
Kosuke Matsuda <kosukematsuda@gmail.com>
2011-01-30 20:04:33 +0900

    create new Rails project with jquery, rspec and without ORM

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plot::Application.config.secret_token = '1b236e6a4ed04dba137df31651340d6de727dcc9be64f54a95954367b9eaf1b46232e8a9f47e89e6303da6e79c184a1faa524b2bf2c6385b10c24d6ce3120867'
